eca6140372919af3d0afafb5cf09a96e0f77acef
amol bagul <amolbagul947@gmail.com>
2025-09-11 12:12:32 +0530

    controlled flow statement

---
+++ Controle_FlowStatement/src/starpattern/InvertedPyaramid.java
@@ -0,0 +1,16 @@
+package starpattern;
+
+public class InvertedPyaramid {
+public static void main(String []args) {
+	int n = 5;
+	for(int i =n;i >=1;i--) {
+		for(int j =i; j<n; j++) {
+			System.out.print(" ");
+		}
+		for(int k =1; k <=(2 * i-1);k++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+}
